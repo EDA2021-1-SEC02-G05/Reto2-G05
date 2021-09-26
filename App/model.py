1971a911d3b1a2739b0e7aaabe22a531ae7b6fb2
Maria Alejandra Moreno Bustillo <m.morenob@uniandes.edu.co>
2021-09-26 16:44:39 -0500

    primero

--- App/model.py
+++ App/model.py
@@ -33,11 +33,86 @@ from DISClib.Algorithms.Sorting import shellsort as sa
 assert cf
 
 """
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
+Se define la estructura de un catálogo de obras y artistas. 
 """
 
 # Construccion de modelos
+def newCatalog():
+    """ Inicializa el catálogo de obras y artistas
+
+    Crea una lista vacia para guardar todos los artistas y todas las obras
+
+    Se crean indices (Maps) por los siguientes criterios:
+    Autores
+    Obras de arte
+    Año de nacimiento de artistas
+    Fecha de adquisición de obras (por año)*
+    Obras de arte de un artista clasificadas por técincas
+    Obras clasificadas por las nacionalidades de los artistas
+
+    Retorna el catalogo inicializado.
+    """
+    catalog = {'Artists': None,
+               'Artworks': None,
+               'ArtistBeginDate': None,
+               'ArtworkDates': None,
+               'Medium': None,
+               'Nationality': None}
+
+    """
+    Estas listas contienen toda la información de las obras de arte y sus artistas.
+    Las listas no se encuentran ordenadas por ningún criterio, pero seran utilizadas como referencia
+    para los mapas que se van a crear
+    """
+    catalog['Artists'] = lt.newList('ARRAY_LIST', compareArtistID)
+    catalog['Artworks'] = lt.newList(('ARRAY_LIST', compareObjectID))
+
+    """
+    A continuacion se crean indices por diferentes criterios
+    para llegar a la informacion consultada.  Estos indices no
+    replican informacion, solo referencian las obras de arte y los artistas
+    que se encuentran en las listas anteriormente creadas
+    """
+
+    """
+    Este indice crea un map cuya llave es el año de nacimiento del artista
+    """
+    catalog['ArtistDates'] = mp.newMap(10000,
+                                   maptype='CHAINING',
+                                   loadfactor=4.0,
+                                   comparefunction=compareMapArtistDate)
+
+    """
+    Este indice crea un map cuya llave es el autor del libro
+    """
+    catalog['authors'] = mp.newMap(800,
+                                   maptype='CHAINING',
+                                   loadfactor=4.0,
+                                   comparefunction=compareAuthorsByName)
+    """
+    Este indice crea un map cuya llave es la etiqueta
+    """
+    catalog['tags'] = mp.newMap(34500,
+                                maptype='PROBING',
+                                loadfactor=0.5,
+                                comparefunction=compareTagNames)
+    """
+    Este indice crea un map cuya llave es el Id de la etiqueta
+    """
+    catalog['tagIds'] = mp.newMap(34500,
+                                  maptype='CHAINING',
+                                  loadfactor=4.0,
+                                  comparefunction=compareTagIds)
+    """
+    Este indice crea un map cuya llave es el año de publicacion
+    """
+    catalog['years'] = mp.newMap(40,
+                                 maptype='PROBING',
+                                 loadfactor=0.5,
+                                 comparefunction=compareMapYear)
+
+    return catalog
+
 
 # Funciones para agregar informacion al catalogo
 
